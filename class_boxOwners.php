a55c2ac514f660725b753e103e060dccdfb442cd
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2023-09-13 14:18:10 +0300

    NO DEFENCES: CRUD updated to check item id

--- class_boxOwners.php
+++ class_boxOwners.php
@@ -7,13 +7,6 @@ class BoxOwner {
         $this->mysql = $conn;   // Connect to the DB
     }
 
-    public function IsInUse($box) {
-        $q  = "SELECT * FROM `postboxes` WHERE BoxNumber ='$box'";
-        $result = mysqli_query($this->mysql, $q);
-
-        return mysqli_num_rows($result) > 0; 
-    }
-
     public function IsPresent($fname, $lname) {
         $q  = "SELECT * FROM `postboxes` ";
         $q .= " WHERE FirstName ='$fname' AND LastName = '$lname'";
@@ -31,10 +24,11 @@ class BoxOwner {
         return mysqli_num_rows($result) > 0; 
     }
 
-    public function SanityCheck($box, $fname, $lname, $phone) {
-        // "nl2br()" function used to support new lines in echoed string:
-        if($this->IsOwner($fname, $lname, $phone)) { echo "The lecturer ".$fname." ".$lname." already owns a box"; }
-        elseif(empty($box)) { echo "Box number should be provided"; }
+    public function SanityCheck($box, $fname, $lname, $phone, $changed) {
+        if($changed && $this->IsOwner($fname, $lname, $phone)) { 
+            echo "The lecturer ".$fname." ".$lname." already owns a box"; 
+        }
+        elseif(empty($box))   { echo "Box number should be provided"; }
         elseif(empty($fname)) { echo "First name should be provided"; }
         elseif(empty($lname)) { echo "Last name should be provided"; }
         elseif(empty($phone)) { echo "Phone number should be provided"; }
@@ -48,26 +42,20 @@ class BoxOwner {
         $box =      isset($params['boxNumber'])     ? $params['boxNumber']      : "";
         $phone =    isset($params['phone'])         ? $params['phone']          : "";
         
-        if($this->IsInUse($box)) { 
-            echo nl2br("The box ".$box." is already in use\r\nTry another one"); 
-            return false;
-        }
-        else {
-            // Check that inputs are acceptable:
-            if($this->SanityCheck($box, $fname, $lname, $phone)) {
-                $q = "INSERT INTO `postboxes` (`FirstName`, `LastName`, `BoxNumber`, `Phone`) ";
-                $q .= " VALUES ('$fname', '$lname', '$box', '$phone')";
+		// Check that inputs are acceptable:
+		if($this->SanityCheck($box, $fname, $lname, $phone, true)) {
+			$q = "INSERT INTO `postboxes` (`FirstName`, `LastName`, `BoxNumber`, `Phone`) ";
+			$q .= " VALUES ('$fname', '$lname', '$box', '$phone')";
 
-                $result = mysqli_query($this->mysql, $q);
-                return true;
-            } 
-            else { return false; }
-        }
+			$result = mysqli_query($this->mysql, $q);
+			return true;
+		} 
+		else { return false; }
     }
 
-    public function GetOwner($box) {
+    public function GetOwner($id) {
         $q  = "SELECT * FROM `postboxes` ";
-        $q .= "WHERE `postboxes`.`BoxNumber` = $box";
+        $q .= "WHERE `postboxes`.`id` = $id";
         $result = mysqli_query($this->mysql, $q);
         $row = mysqli_fetch_assoc($result);
         return $row;
@@ -90,16 +78,17 @@ class BoxOwner {
     public function UpdateOwner($params) {
         $fname = isset($params['fname']) ? $params['fname'] : "";
         $lname = isset($params['lname']) ? $params['lname'] : "";
-        $box =   isset($params['box']) ? $params['box'] : -1;
         $phone = isset($params['phone']) ? $params['phone'] : "";
+        $box =   isset($params['box'])   ? $params['box'] : "";
+        $id =    isset($params['id'])    ? $params['id'] : -1;
 
-        if($box > 0) {
+        if($id > 0) {
             $q = "UPDATE `postboxes` SET ";
             $q .= " `FirstName` = '$fname', ";
             $q .= " `LastName` = '$lname', ";
             $q .= " `Phone` = '$phone', ";
             $q .= " `BoxNumber` = '$box' ";
-            $q .= " WHERE `postboxes`.`BoxNumber` = $box ";
+            $q .= " WHERE `postboxes`.`id` = $id ";
 
             $result = mysqli_query($this->mysql, $q);
             return true;
@@ -108,12 +97,11 @@ class BoxOwner {
     }
 
     public function DeleteOwner($params) {
-        $box = isset($params['btnDelete']) ? $params['btnDelete'] : "";
+        $id = isset($params['btnDelete']) ? $params['btnDelete'] : "";
 
         $q = "DELETE FROM `postboxes` ";
-        $q .= "WHERE `postboxes`.`BoxNumber` = '$box'";
+        $q .= "WHERE `postboxes`.`id` = '$id'";
 
         $result = mysqli_query($this->mysql, $q);
     }
-
 }
