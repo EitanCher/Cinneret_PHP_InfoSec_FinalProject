700c8d1dc55f77f9b134a0ddd1202bac09792a46
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2023-09-13 14:30:35 +0300

    NO DEFENCES: CRUD updated to check item id

--- CRUD_Update.php
+++ CRUD_Update.php
@@ -7,14 +7,18 @@ include "class_boxOwners.php";
 $myObj = new BoxOwner($mysql);
 
 if(isset($_GET['btnEdit'])) {
-    if($myObj->SanityCheck($_GET['box'], $_GET['fname'], $_GET['lname'], $_GET['phone'])) {
+    // Check if owner's details changed or not:
+	$isChanged = $_GET['fname'] != row['FirstName'] || $_GET['lname'] != row['LastName'] || $_GET['phone'] != row['Phone'];
+
+	if($myObj->SanityCheck($_GET['box'], $_GET['fname'], $_GET['lname'], $_GET['phone'], $isChanged)) {
         $myObj->UpdateOwner($_GET);
         header("location:./CRUD_Read_Delete.php");
     }
 }
 
-$box = isset($_GET['rbox']) ? $_GET['rbox'] : -1;  //"rbox" is passed by URL from "EDIT" link in "Read" page
-$row = $myObj->GetOwner($box);
+$id = isset($_GET['rid']) ? $_GET['rid'] : -1;  //"rid" is passed by URL from "EDIT" link in "Read" page
+$row = $myObj->GetOwner($id);
+
 ?>
 
 <!DOCTYPE html>
@@ -28,9 +32,10 @@ $row = $myObj->GetOwner($box);
 </head>
 <body>
     <div id="container">    
-        <h2>UPDATE OWNER OF BOX #<?= $box ?></h2>
+        <h2>UPDATE DETAILS</h2>
         <form action="" method="get">
-            <input type="hidden" name="box" value="<?= $box ?>" readonly/>
+            <input type="hidden" name="id" 	value="<?= $id ?>"/><br>
+            <input type="text" name="box" 	value="<?= $row['BoxNumber'] ?>"/><br>
             <input type="text" name="fname" value="<?= $row['FirstName'] ?>"/><br>
             <input type="text" name="lname" value="<?= $row['LastName'] ?>"/><br>
             <input type="text" name="phone" value="0<?= $row['Phone'] ?>"/><br>
